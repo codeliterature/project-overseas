ea3a2d015e106c6cf9dc5f94149234b6436f56a1
inayatbakshi46 <inayatbakshi75@gmail.com>
2023-09-12 10:27:48 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(".fa-solid").click(function() {
+  $(".nav-links").toggleClass("hidden");
+  $("body").toggleClass("open-menu");
+  $(".navlinks").fadeOut(100).fadeIn(100);
+});
+
+$(".ham-links").click(function() {
+  $(".nav-links").addClass("hidden");
+  $("body").removeClass("open-menu");
+});
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  $(".fa-thin").toggleClass("fa-sun");
+  $(".fa-thin").toggleClass("fa-moon");
+  $(".nav-links").addClass("hidden");
+  $("body").removeClass("open-menu");
+}
+
+
+$(".btn").click(function() {
+  $(".contea").toggleClass("hidden");
+  $("body").toggleClass("open-menu");
+});
+
+$(".cls").click(function() {
+  $(".contea").addClass("hidden");
+  $("body").removeClass("open-menu");
+  
+});
